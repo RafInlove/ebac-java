412a279bcc5130a6b46d17cba3a82167fc92eded
R <raphael18pro@gmail.com>
2024-07-09 18:50:07 -0300

    finalizando exercício

---
+++ tarefa-wrapper/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int varP = 5;
+        System.out.println("a variável primitiva varP é igual a " +varP);
+        Integer varI = varP;
+        System.out.println("Agora é um wrapper, mas ainda vale " +varI);
+    }
+}
